5015527f13e6d9dc13d3fc8394f9f66d15ef052d
Cristhian Cabezas <cristhiancabezas9@gmail.com>
2020-08-13 12:37:51 -0500

    inicio

---
+++ semaforo.js
@@ -0,0 +1,102 @@
+    const semaforo = {
+      "rojo": {
+        "color": "#FF0000",
+        "restricciones": [
+          {
+            "id": 1,
+            "descripcion": "El horario del toque de queda es de 18:00 a 05:00. "
+          },
+          {
+            "id": 2,
+            "descripcion": "Los vehículos particulares, incluidas las motos, no pueden circular los fines de semana. "
+          },
+          {
+            "id": 3,
+            "descripcion": "Transporte mixto pueden circular según la placa para e impar. "
+          },
+          {
+            "id": 4,
+            "descripcion": "Taxis circulan en función del número de placa par e impar los sábados, el domingo circulan todos. "
+          },
+          {
+            "id": 5,
+            "descripcion": "Transporte institucional circula sin restricción de placa. "
+          },
+          {
+            "id": 6,
+            "descripcion": "Transporte provincial no puede circular. "
+          }
+        ]
+      },
+      "amarillo": {
+        "color": "#FFFF00",
+        "restricciones": [
+          {
+            "id": 1,
+            "descripcion": "El horario de toque de queda es de 21:00 a 05:00. "
+          },
+          {
+            "id": 1,
+            "descripcion": "El sábado pueden circular los autos cuya placa termina en número par. El domingo no circulan los vehículos particulares. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Transporte mixto pueden circular según la placa par e impar. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Taxis circulan de acuerdo a la placa par e impar los sábado, el domingo circulan todos. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Transporte institucional circula sin restricción de placa. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Transporte urbano e interparroquial puede circular con el 50% de su aforo. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Transporte intracantonal puede circular entre cantones con el mismo color del semáforo, también con el 50% de su capacidad. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Transporte interprovincial solo entre cantones de provincias colindantes. "
+          }
+        ]
+      },
+      "verde": {
+        "color": "#00FF00",
+        "restricciones": [
+          {
+            "id": 1,
+            "descripcion": "El toque de queda es de 24:00 a 05:00. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Vehículos particulares no circulan el 30%. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Taxis y transporte mixtos circulan todos. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Transporte institucional circula sin restricción de placa. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Transporte urbano e interparroquial circula con el 50% de su aforo o con todos los pasajeros sentados. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Transporte intracantonal puede circular entre cantones con el mismo color del semáforo, también con el 50% de su capacidad. "
+          },
+          {
+            "id": 1,
+            "descripcion": "Transporte interprovincial solo entre cantones de provincias colindantes."
+          }
+        ]
+      }
+    }
+  
